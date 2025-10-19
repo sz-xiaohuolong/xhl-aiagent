1cdb376e793107bfe8435f5953ba9cc17f924617
xhl <17706212405@163.com>
2025-10-19 16:12:19 +0800

    初始化智能体项目，并接入大模型

---
+++ src/main/java/com/xhl/xhlaiagent/XhlAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xhl.xhlaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhlAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhlAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhl/xhlaiagent/XhlAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhl.xhlaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhlAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
