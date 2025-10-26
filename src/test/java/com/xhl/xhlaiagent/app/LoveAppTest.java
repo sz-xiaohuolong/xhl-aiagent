bc3261795a633235c98987c08d4ed2efd021977c
xhl <17706212405@163.com>
2025-10-26 17:34:44 +0800

    实现结构化输出及自定义基于文件的对话记忆

---
+++ src/test/java/com/xhl/xhlaiagent/app/LoveAppTest.java
@@ -0,0 +1,43 @@
+package com.xhl.xhlaiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是小火龙，你的底层是什么模型呀";
+        String answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第二轮
+        message = "我想让另一半（小蛋糕）更爱我";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+
+    @Test
+    void doChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是小火龙，我想让另一半（小蛋糕）更爱我，但我不知道该怎么做";
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(loveReport);
+    }
+}
